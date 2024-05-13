32fb26a7e305daf2dfe8db59755b4fe0840efa4e
Ainur Yarulin <yarulinainur@gmail.com>
2024-05-13 11:06:45 +0700

    🚀

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -1,6 +1,10 @@
 import { clsx, type ClassValue } from 'clsx'
 import { twMerge } from 'tailwind-merge'
 
+/**
+ ** Эту утилиту рекомендуется использовать для объявления классов
+ ** Например: const className = `cn('text-red-500', 'border-red-500', { 'text-blue-500': true })`
+ */
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs))
 }
